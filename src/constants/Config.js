81186f7c11811409394b716a4a61315c6f564b04
nvanha <nvha1120@gmail.com>
2021-08-06 00:16:19 +0700

    complete pages/NotFoundPage, and test remove outline blue in safari

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,1 +1,2 @@
 export const HOME_PAGE = "nobita_home";
+export const API_URL = "http://localhost:3000";
--- src/index.js
+++ src/index.js
@@ -9,14 +9,18 @@ import "./sass/index.scss";
 
 import Layout from "./components/Layout/Layout";
 
+// Redux
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Reducers from "./reducers/index";
+
+const store = createStore(Reducers);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Layout />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
